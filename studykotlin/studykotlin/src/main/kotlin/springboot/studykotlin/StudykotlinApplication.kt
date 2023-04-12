76d87a1b3f2fd11a141cdaed2af6d0f59e4580fe
lkc263 <lkc263@naver.com>
2023-04-13 01:15:35 +0900

    no message

---
+++ studykotlin/studykotlin/src/main/kotlin/springboot/studykotlin/StudykotlinApplication.kt
@@ -0,0 +1,11 @@
+package springboot.studykotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudykotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudykotlinApplication>(*args)
+}
